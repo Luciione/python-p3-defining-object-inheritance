389efb4a5a14c7d0cfb95a4c63abc54c2b21d1e6
+ <lucy.njoroge1@student.moringaschool.com>
2023-08-25 03:36:36 +0300

    completed

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,20 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+
+    def fill_up_tank(self):
+        return "filling up!"
+
+    def honk(self):
+        return "HONK HONK"
+
+    def drive_fast(self):
+        return "Driving so fast"
+
+    def is_motorized(self):
+        return True
+
+    def num_wheels(self):
+        return self.wheel_number
